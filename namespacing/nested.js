9435da088f374293ec74d2b0bcf85f546e73fe57
Ed Shadi <edshadi@gmail.com>
2013-11-07 14:13:39 -0800

    nested.

---
+++ namespacing/nested.js
@@ -0,0 +1,10 @@
+var myApp =  myApp || {};
+myApp.models = {
+  views: {
+    pages: {}
+  }
+}
+// OR
+myApp.models = myApp.models || {};
+myApp.models.views = myApp.models.views || {};
+myApp.models.views.pages = myApp.models.views.pages || {};
